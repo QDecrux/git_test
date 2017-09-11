93f81aa37c568f778112e6ad44e02f7c5975c572
Uladzimir Marchenko <MarchenkoFromMinsk@gmail.com>
2017-09-11 13:22:41 +0300

    Main menu added in main();

--- git_test/main.cpp
+++ git_test/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
+
+void start_game()
+{
+
+}
+
+
 int main()
 {
-	printf("Press any key to continue");
+	while (true)
+	{
+		system("cls");
+		printf("\t\tWelcome to labyrynth!\n\n\n");
+		printf("1\t- Start game\n");
+		printf("0\t- Exit\n");
+		rewind(stdin);
+		char c = std::cin.get();
+		switch (c)
+		{
+		case '1':
+			start_game();
+			break;
+
+		case '0':
+			goto end;
+		}
+	}
+end:
+
+	system("cls");
+	printf("The end! Press any key to continue...");
+	rewind(stdin);
 	system("pause > NUL");
 	return 0;
 }
